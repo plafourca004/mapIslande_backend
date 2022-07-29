239e5cf4da8e73bc77756441f32f9b9e21998aeb
plafourca004 <paul64lafourcade@orange.fr>
2022-07-29 20:31:36 +0200

    Back end V2 that uses AWS S3

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
-const express = require('express');
+const AWS = require("aws-sdk")
 const multer = require("multer")
+const multerS3 = require("multer-s3")
+
+const express = require('express');
 const cors = require('cors');
 const app = express();
 
@@ -10,8 +13,6 @@ const BASE_URL = process.env.BASE_URL
 
 const JSONdb = require('simple-json-db')
 const db = new JSONdb('./db.json')
-
-const upload = multer({dest: 'uploads/'})
 
 app.use(cors());
 
@@ -22,14 +23,34 @@ app.use(function (req, res, next) {
 });
 
 
-app.post(BASE_URL + "/points/", upload.single('img'), (req, res) => {
-    let dbMarkers = db.get("points")
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const uploadS3 = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.BUCKET_NAME,
+        metadata: function (req, file, cb) {
+          cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+          cb(null, Date.now().toString())
+        }
+    })
+})
+
+
+app.post(BASE_URL + "/points/", uploadS3.single("img"), (req, res) => {
+    let dbMarkers = db.get("points")    
+    console.log(req.file)
+
     if (dbMarkers == null || dbMarkers == undefined || dbMarkers.length == 0) dbMarkers = []
-    let newMarker = {"id" : dbMarkers.length,  "lat": req.body.lat, "lng": req.body.lng, "date": req.body.date, "notes": req.body.notes, "filename": req.file.filename, "tag": req.body.categorie}
+    let newMarker = {"id" : dbMarkers.length,  "lat": req.body.lat, "lng": req.body.lng, "date": req.body.date, "notes": req.body.notes, "filename": req.file.originalname, "tag": req.body.categorie, "link": req.file.location }
     dbMarkers.push(newMarker)
 
     db.set("points", dbMarkers)
-    console.log(newMarker)
     return res.status(201).json({"message" : "201 worked"})
 })
 
@@ -39,14 +60,9 @@ app.get(BASE_URL + "/points/", (req, res) => {
     res.status(200).json(dbMarkers)
 })
 
-app.get(BASE_URL + "/image/:file", (req, res) => {
-    let filename = req.params.file
-    let location = '/uploads/' + filename
-    res.sendFile(process.env.LOCATION_BACKEND + location)
-})
-
 app.get(BASE_URL + "/tags/", (req, res) => {
     let dbMarkers= db.get("points")
+    if (dbMarkers == null || dbMarkers == undefined || dbMarkers.length == 0) dbMarkers = []
     let tags = []
     for(i = 0; i < dbMarkers.length; i++) {
         if (!tags.includes(dbMarkers[i].tag)) {
@@ -55,6 +71,7 @@ app.get(BASE_URL + "/tags/", (req, res) => {
     }
     res.status(200).json(tags)
 })
+
 
 app.listen(port, () => {
     console.log('App listening on port + ' + port);
